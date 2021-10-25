04757e80162bfce6b0565c913b36f3856fb07146
mattladany <matt@va-a.com>
2021-10-25 19:49:34 +0000

    Add superficial String() function to Circuit struct

--- quantum/circuit.go
+++ quantum/circuit.go
@@ -1,27 +1,55 @@
 package quantum
 
 import (
+    "bytes"
     "errors"
     "fmt"
+    "strconv"
 )
 
 // Circuit a quantum circuit that contains qubits and quantum gates.
 type Circuit struct {
-    size   int
+    dimensions int
     qubits []*Qubit
+    gates [][2][2]complex128
 }
 
 // MakeCircuit creates a Circuit with the provided qubits.
-func MakeCircuit(size int, qubits ...*Qubit) (*Circuit, error) {
+func MakeCircuit(dimensions int, qubits ...*Qubit) (*Circuit, error) {
     circuit := new(Circuit)
-    if size != len(qubits) {
-        return nil, errors.New(fmt.Sprintf("circuit size: %d does not match number of qubits: %d", size, len(qubits)))
+    if dimensions != len(qubits) {
+        return nil, errors.New(fmt.Sprintf("circuit size: %d does not match number of qubits: %d", dimensions, len(qubits)))
     }
-    circuit.size = size
+    circuit.dimensions = dimensions
 
     for _, qubit := range qubits {
         circuit.qubits = append(circuit.qubits, qubit)
     }
 
     return circuit, nil
+}
+
+// MakeSingleQubitCircuit creates a Circuit with a single Qubit that has the specified gates.
+func MakeSingleQubitCircuit(qubit *Qubit, gates ...[2][2]complex128) (*Circuit, error) {
+    circuit := new(Circuit)
+    circuit.dimensions = 1
+    circuit.qubits = append(circuit.qubits, qubit)
+    for _, gate := range gates {
+        circuit.gates = append(circuit.gates, gate)
+    }
+    return circuit, nil
+}
+
+// String returns a string representation of the qubits, gates, and their interactions currently loaded in this circuit.
+func (circuit Circuit) String() string {
+    var buffer bytes.Buffer
+    buffer.WriteString(fmt.Sprintf("[%v|%0*s> + ", circuit.qubits[0].basis0, 1, strconv.FormatInt(int64(0), 2)))
+    buffer.WriteString(fmt.Sprintf("%v|%0*s>]---", circuit.qubits[0].basis1, 1, strconv.FormatInt(int64(1), 2)))
+    // TODO write function to compare gate slices and return a string representation of the gate
+//    for _, _ = range circuit.gates {
+//        buffer.WriteString(fmt.Sprintf("%s---", "X"))
+//    }
+
+
+    return buffer.String()[:len(buffer.String())-3]
 }
--- quantum/circuit_test.go
+++ quantum/circuit_test.go
@@ -2,6 +2,7 @@ package quantum
 
 import (
     "testing"
+    "fmt"
 )
 
 func TestMakeCircuit1Qubit1Size(t *testing.T) {
@@ -26,4 +27,19 @@ func TestMakeCircuitInvalid1Qubit2Size(t *testing.T) {
     if err == nil {
         t.Fatalf("circuit should not have been created, but was")
     }
+}
+
+func TestMakeSingleQubitCircuitWith1Gate(t *testing.T) {
+    qubit, err1 := MakeQubit(1, 0)
+    if err1 != nil {
+        t.Fatalf(err1.Error())
+    }
+
+    x := MakeXGate()
+    circuit, err2 := MakeSingleQubitCircuit(qubit, x)
+    if err2 != nil {
+        t.Fatalf(err2.Error())
+    }
+
+    fmt.Println(circuit)
 }
